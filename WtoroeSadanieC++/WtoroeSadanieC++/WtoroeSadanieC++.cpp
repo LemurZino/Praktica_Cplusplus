851f3bd7ebc0bca4f7dec03efadac2db1e005b57
LemurZino <danya.danila2014.danya@mail.ru>
2020-01-20 12:26:33 +0300

    veria v1

---
+++ WtoroeSadanieC++/WtoroeSadanieC++/WtoroeSadanieC++.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <map>
+
+using namespace std;
+
+void remove_all(multimap<int, string> myMap) {
+
+
+    for (auto it = (++myMap.begin()); it != myMap.end();it++) {
+        if (it->second == "Vasya") {
+            it = myMap.erase(it);
+            continue;
+        }
+        cout << it->first << " : " << it->second << endl;
+    }
+}
+
+int main()
+{
+    multimap<int, string> myMap = { {1, "Vasya"},
+                                       {2, "Vasya"},
+                                       {3, "Vasya"},
+                                       {4, "Nikita"},
+                                       {5, "Kolya"},
+                                       {6, "Katya"},
+                                       {7, "Tanja"},
+                                       {8, "Ilja"},
+                                       {9, "Sergey"},
+                                       {10, "Mitrofan"} };
+    remove_all(myMap);
+}
+
+
+
+
+
